d715d2aaa1678fc496eaa8aebd0c7b961dd2355c
Florencia Luz Duarte <florluzduarte@gmail.com>
2022-11-09 11:56:01 -0300

    desafio terminado

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listEl = document.querySelector(".lista");
+
+  const liElsOriginals = listEl.querySelectorAll("li");
+  liElsOriginals.forEach((element) => {
+    element.remove();
+  });
+
+  cosasQueAprendimos.forEach((element) => {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = element.tema;
+    newLiEl.classList.add(element.class || "new");
+    listEl.appendChild(newLiEl);
+  });
+}
 
 main();
